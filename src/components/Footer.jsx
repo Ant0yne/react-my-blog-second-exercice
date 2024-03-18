627f81e42a9372a15778761458ea70dbdadd7e02
Ant0yne <antoine.soliv@gmail.com>
2024-03-18 22:00:19 +0100

    a-target

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,8 +5,13 @@ const Footer = () => {
 				<div>
 					<p>
 						Made with <span>React</span> at{" "}
-						<a href="https://www.lereacteur.io/">Le Reacteur</a> by{" "}
-						<a href="https://www.linkedin.com/in/antoine-soliveres-396811a4">
+						<a href="https://www.lereacteur.io/" target="_blank">
+							Le Reacteur
+						</a>{" "}
+						by{" "}
+						<a
+							href="https://www.linkedin.com/in/antoine-soliveres-396811a4"
+							target="_blank">
 							Antoine SOLIVERES
 						</a>
 					</p>
